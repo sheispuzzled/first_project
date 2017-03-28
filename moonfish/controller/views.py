18d743689cec2ab124fe9bcf4e5af4abba2921e9
jihyeok <sheispuzzled@gmail.com>
2017-03-29 00:41:58 +0900

    add index.html

--- moonfish/controller/views.py
+++ moonfish/controller/views.py
@@ -7,3 +7,8 @@ from flask import request, jsonify, render_template
 @app.route('/', methods=['GET'])
 def home():
     return 'Connection Success'
+
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
